483e1d03c721ef6e62973791c32c9e462df3143d
leydevitm <leivy.candelaria@devitm.com>
2025-06-09 17:26:27 -0600

    Clase 31 Inicio del Proyecto

---
+++ seccion4/app1.js
@@ -0,0 +1,12 @@
+
+console.clear();
+console.log('==============');
+console.log(`tabla del ${base}`);
+console.log('==============');
+
+const base =5;
+for (let index = 1; index <= 10; index++) {
+   
+console.log(`${base} *${index} = ${base * index}`);
+    
+}
