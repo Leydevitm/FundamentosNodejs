ec7f69a4a600ab1efb7313129d18e5d0a9649627
leydevitm <leivy.candelaria@devitm.com>
2025-06-10 15:18:55 -0600

    Clase 38 Configuracion de yargs independientes

--- seccion4/app2.js
+++ seccion4/app2.js
@@ -1,37 +1,7 @@
 const { crearArchivo } = require('./helpers/multiplicar'); 
-const { hideBin } = require('yargs/helpers');
-const yargs = require('yargs');
-
-const argv = yargs(hideBin(process.argv))
-    .option('b', {
-        alias: 'base',
-        type: 'number',
-        demandOption: true
-        
-    })
-    .option('l', {
-        alias: 'listar',
-        type: 'boolean',
-        demandOption: true,
-        default: false,
-        
-    })
-    .check((argv, options) => {
-        if (isNaN(argv.b)) {
-            throw 'La base tiene que ser un número'
-        }
-        return true; 
-    })
-    .argv;
-
+const argv= require('./config/yargs');
 
 console.clear();
-console.log(argv);
-//console.log('base: yargs', argv.b);
-
-
-//const base = 3;
 
 crearArchivo(argv.b, argv.l) 
 .then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
-//.catch((err) => console.log(err));
---
+++ seccion4/config/yargs.js
@@ -0,0 +1,28 @@
+
+const { hideBin } = require('yargs/helpers');
+const yargs = require('yargs');
+
+const argv = yargs(hideBin(process.argv))
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true,
+        describe: 'Es la base de la tabla de multiplicar'
+        
+    })
+    .option('l', {
+        alias: 'listar',
+        type: 'boolean',
+        default: false,
+        describe: 'Muestra la tabla en consola'
+        
+    })
+    .check((argv, options) => {
+        if (isNaN(argv.b)) {
+            throw 'La base tiene que ser un número'
+        }
+        return true; 
+    })
+    .argv;
+
+    module.exports= argv;
