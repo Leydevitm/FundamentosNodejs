ebcdce6b83821b644fb8ad3eaaf9ed94d69aed3c
leydevitm <leivy.candelaria@devitm.com>
2025-06-10 15:01:48 -0600

    Clase 27 Configuracion de Yargs

--- seccion4/app2.js
+++ seccion4/app2.js
@@ -1,17 +1,37 @@
-const {crearArchivo}=require('./helpers/multiplicar'); 
+const { crearArchivo } = require('./helpers/multiplicar'); 
+const { hideBin } = require('yargs/helpers');
+const yargs = require('yargs');
+
+const argv = yargs(hideBin(process.argv))
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true
+        
+    })
+    .option('l', {
+        alias: 'listar',
+        type: 'boolean',
+        demandOption: true,
+        default: false,
+        
+    })
+    .check((argv, options) => {
+        if (isNaN(argv.b)) {
+            throw 'La base tiene que ser un número'
+        }
+        return true; 
+    })
+    .argv;
+
 
 console.clear();
-
-console.log(process.argv);
-const [,,arg3='base=5']=process.argv;
-const [,base=5]=arg3.split('=');
-console.log(base);
-
-
+console.log(argv);
+//console.log('base: yargs', argv.b);
 
 
 //const base = 3;
 
-crearArchivo(base) 
+crearArchivo(argv.b, argv.l) 
 .then((nombreArchivo) => console.log(nombreArchivo, 'creado'))
-.catch((err) => console.log(err));
+//.catch((err) => console.log(err));
--- seccion4/helpers/multiplicar.js
+++ seccion4/helpers/multiplicar.js
@@ -1,20 +1,22 @@
 
 const fs=require('fs');
-const crearArchivo= async (base=5 )=>{
+const crearArchivo= async (base=5, listar=false)=>{
 
     try {
-       console.log('==============');
-console.log(`Tabla del ${base}`);
-console.log('==============');
-
-let salida='';
+    let salida='';
 
 for (let index = 1; index <= 10; index++) {
-   
     salida +=(`${base} *${index} = ${base * index}\n`);
     
-}
-console.log(salida);
+    }
+
+ if(listar){
+    console.log('==============');
+    console.log(`Tabla del ${base}`);
+    console.log('==============');
+    console.log(salida);
+        }
+
 fs.writeFileSync(`tabla-${base}.txt`,salida);
    
 
