6bbec9f68af432692ddf3fe4cb28e3581a41dcc8
leydevitm <leivy.candelaria@devitm.com>
2025-06-10 20:33:06 -0600

    Clase 48 stadin-stdout-readline

--- seccion5/app.js
+++ seccion5/app.js
@@ -1,9 +1,13 @@
-
 require('colors');
+const { mostrarMenu,pausa } = require('./helpers/mensajes');
+
+  console.clear();
 
 const main =async()=>{
-   console.clear();
+ 
     console.log('hola mundo');
+    mostrarMenu();
+    pausa();
 }
 
 main();
