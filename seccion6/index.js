d6d7e71cc32daaf83113426bcd98d27f092a4225
leydevitm <leivy.candelaria@devitm.com>
2025-06-13 16:53:50 -0600

    Clase 71 Modelo para controlar la aplicacion

--- seccion6/index.js
+++ seccion6/index.js
@@ -1,6 +1,7 @@
 
 require('colors');
 const { inquirerMenu,pausa,leerInput } = require('./helpers/menu');
+const Busquedas = require('./models/busquedas');
 
 
 
@@ -9,13 +10,35 @@ const { inquirerMenu,pausa,leerInput } = require('./helpers/menu');
 const main =async()=>{
  
     let opt;
+    const busquedas = new Busquedas();
+    
    
     do {
        opt = await inquirerMenu();
-      console.log({opt});
+       switch(opt ){
+        case 1:
+            //mostrar mensaje 
+            const lugar =await leerInput('Ciudad:' );    
+            console.log(lugar);        //buscar los lugar 
+            //seleccionar el lugar 
+
+            //datos del clima
+
+            //Mostrar resultados 
+            console.log('\nInformacion de la ciudad\n'.green);
+            console.log('Ciudad: ',)
+            console.log('Lat: ',)
+            console.log('Lng: ',)
+            console.log('Temperatura: ',)
+            console.log('Minima: ',)
+            console.log('Maxima: ',)
+            break;
+
+       }
+     
        
        
-        await pausa();
+    if(opt!==0) await pausa();
        
     } while (opt !==0)
     
---
+++ seccion6/models/busquedas.js
@@ -0,0 +1,24 @@
+
+class Busquedas{
+    historial=['Hidalgo,Madrin,San Jose']
+
+    constructor(){
+
+    }
+
+    async ciudad(lugar=''){
+
+        //peticion http
+        console.log(lugar);
+
+        return[];
+    }
+
+    
+
+
+}
+
+
+
+module.exports= Busquedas;
