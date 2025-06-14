accf2c2ee8db590dcc8737bf2a28ec41b1da2af3
leydevitm <leivy.candelaria@devitm.com>
2025-06-13 18:38:20 -0600

    Clase 75 Mapbox search api y token de acceso

--- seccion6/models/busquedas.js
+++ seccion6/models/busquedas.js
@@ -11,7 +11,7 @@ class Busquedas{
         try {
             //peticion http
             // console.log('ciudad', lugar);
-            const resp =await axios.get('https://reqres.in/api/users?page=2');
+            const resp =await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/colo.json?bbox=-180%2C-90%2C180%2C90&limit=5&proximity=-73.990593%2C40.740121&types=country%2Cregion&language=es&access_token=pk.eyJ1IjoibGV5NG4tZGV2IiwiYSI6ImNtYnZncTlrdzBvbWIybG9mOHUxejdjYncifQ.n7k3o2fsdjFnkRmKoqbsOg');
             console.log(resp.data);
             return []
         } catch (error) {
