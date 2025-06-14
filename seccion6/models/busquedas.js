02deb42ab9a708c0ab0cfb1c5ab9cebf36610f2a
leydevitm <leivy.candelaria@devitm.com>
2025-06-13 19:23:22 -0600

    Clase 76 Crear instancias de Axios

--- seccion6/models/busquedas.js
+++ seccion6/models/busquedas.js
@@ -5,14 +5,26 @@ class Busquedas{
     constructor(){
 
     }
+    get paramsMapbox(){
+        return {
+            'access_token': 'pk.eyJ1IjoibGV5NG4tZGV2IiwiYSI6ImNtYnZncTlrdzBvbWIybG9mOHUxejdjYncifQ.n7k3o2fsdjFnkRmKoqbsOg',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+    
 
     async ciudad(lugar=''){
 
         try {
             //peticion http
-            // console.log('ciudad', lugar);
-            const resp =await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/colo.json?bbox=-180%2C-90%2C180%2C90&limit=5&proximity=-73.990593%2C40.740121&types=country%2Cregion&language=es&access_token=pk.eyJ1IjoibGV5NG4tZGV2IiwiYSI6ImNtYnZncTlrdzBvbWIybG9mOHUxejdjYncifQ.n7k3o2fsdjFnkRmKoqbsOg');
+            const intance =axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            });
+            const resp = await intance.get();
             console.log(resp.data);
+        
             return []
         } catch (error) {
             console.log('No se encontro nads')
