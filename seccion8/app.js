fad6944c83e924a772c22ca599f51987f8b6ca0f
leydevitm <anelgarcia764@gmail.com>
2025-06-17 18:23:33 -0600

    Clase 107 Express basado en clases RESTServer

--- seccion8/app.js
+++ seccion8/app.js
@@ -1,14 +1,7 @@
+
 require('dotenv').config();
+const Server = require('./models/server');
 
-const express = require('express')
-const app = express()
+const server = new Server();
 
-
-app.get('/',(req,res)=> {
-res.send('Hello World')
-
-});
-
-app.listen(process.env.PORT, ()=>{
-    console.log('Servidor corriendo en el puerto', process.env.PORT);
-});
+server.listen();
