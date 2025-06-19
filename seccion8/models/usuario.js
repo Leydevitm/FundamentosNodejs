4377bc64589f5f7e744f86bd5546d335fa09f537
leydevitm <anelgarcia764@gmail.com>
2025-06-18 18:49:55 -0600

    Clase 127 Modelos de usuario

---
+++ seccion8/models/usuario.js
@@ -0,0 +1,37 @@
+
+const{ Schema, model } = require('mongoose');
+
+const usuarioSchema = Schema({
+    name:{
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email:{
+        type: String,
+        required: [true, 'El correo es obligatorio'],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true, 'La contraseña es obligatoria']
+    },
+    img:{
+        type: String,
+        
+    },
+    role:{
+        type: String,
+        enum: ['USER_ROLE', 'ADMIN_ROLE'],
+        default: 'USER_ROLE'
+    },
+    estado:{
+        type: Boolean,
+        default: true
+    },
+    google:{
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = model('Usuario', usuarioSchema);
