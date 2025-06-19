16cac980f44b725cf56a914b0135d245771cbf62
leydevitm <anelgarcia764@gmail.com>
2025-06-18 22:37:49 -0600

    Clase 130 Validar campos obligatorio Email

--- seccion8/controllers/usuarios.js
+++ seccion8/controllers/usuarios.js
@@ -1,6 +1,7 @@
 const {response , request} = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
+const { validationResult } = require('express-validator');
 
 
 const usuariosGet = (req = request,res = response)=> {
@@ -24,10 +25,21 @@ const usuariosPut = (req,res = response)=> {
 }
 
 const usuariosPost = async(req,res = response)=> {
+
+    const errors= validationResult(req);
+    if( !errors.isEmpty()){
+       return res.status(400).json(errors);
+    }
+
     const {name,email,password,role} = req.body;
     const usuario = new Usuario({name,email,password,role});
     //Verificar si el correo existe
-
+    const existeEmail = await Usuario.findOne({email});
+    if(existeEmail){
+        return res.status(400).json({
+            msg: 'El correo ya está registrado'
+        });
+    }
     //Ecriptar la contraseña 
     const salt = bcryptjs.genSaltSync(10);
     usuario.password=bcryptjs.hashSync(password,salt);
--- seccion8/routes/usuarios.js
+++ seccion8/routes/usuarios.js
@@ -1,12 +1,15 @@
 
 const{ Router } = require('express');
-const { usuariosGet, usuariosPost,usuariosPut,usuariosDelete,usuariosPatch } = require('../controllers/usuarios.js');
 
+const { check } = require('express-validator');
+const { usuariosGet, usuariosPost,usuariosPut,usuariosDelete,usuariosPatch } = require('../controllers/usuarios.js');
 const router = Router();
 
 router.get('/',usuariosGet);
 router.put('/:id',usuariosPut);
-router.post('/',usuariosPost);
+router.post('/',[
+    check('email','El correo no es válido').isEmail(),
+],usuariosPost);
 router.delete('/',usuariosDelete);
 router.patch('/',usuariosPatch);
 
