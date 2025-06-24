81064c140f128744dcd037f19a968ee10c636a8e
leydevitm <anelgarcia764@gmail.com>
2025-06-24 15:40:12 -0600

    Clase 188 Buscar en otras colecciones

--- seccion8/controllers/buscar.js
+++ seccion8/controllers/buscar.js
@@ -4,7 +4,7 @@ const {Usuario,Categoria,Producto} = require('../models');
 const { isValidObjectId } = require('mongoose');
 const coleccionesPermitidas =[
     'usuarios',
-    'categoria',
+    'categorias',
     'productos',
     'roles'
 ]
@@ -32,6 +32,44 @@ const buscarUsuarios = async(termino='', res=response)=>{
 
 }
 
+const buscarCategorias = async ( termino='', res=response)=>{
+    const esMongoID = isValidObjectId(termino);
+
+    if(esMongoID){
+        const categoria = await Categoria.findById(termino);
+        return res.json({
+            results:(categoria) ?[categoria] : []
+        })
+    }
+    const regex = new RegExp(termino, 'i');
+    //find regresa vacio en caso de no haber nada 
+    //cout para contar cuentas respuestas resivimos 
+    const categrias = await Categoria.find({ nombre: regex,estado:true});
+     res.json({
+            results: categrias
+        });
+}
+
+const buscarProductos = async (termino ='', res=response)=>{
+    const esMongoID = isValidObjectId(termino);
+
+    if(esMongoID){
+        const producto = await Producto.findById(termino)
+        .populate('categoria', 'nombre');
+        return res.json({
+            results:(producto) ?[producto] : []
+        })
+    }
+    const regex = new RegExp(termino, 'i');
+    //find regresa vacio en caso de no haber nada 
+    //cout para contar cuentas respuestas resivimos 
+    const productos = await Producto.find({nombre:regex, estado:true })
+    .populate('categoria', 'nombre');
+     res.json({
+            results: productos
+        });
+}
+
 const buscar = (req,res=response)=>{
     const {coleccion, termino} = req.params;
 
@@ -45,9 +83,10 @@ const buscar = (req,res=response)=>{
         case 'usuarios':
             buscarUsuarios(termino,res);
             break;
-         case 'categoria':
-            break;
+         case 'categorias':
+            buscarCategorias(termino,res);            break;
          case 'productos':
+            buscarProductos(termino, res);
             break;
         default:
             res.status(500).json({
