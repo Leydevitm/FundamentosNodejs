ee6389bffd823524cd811d1fdda01076159065a1
leydevitm <anelgarcia764@gmail.com>
2025-06-24 21:21:00 -0600

    Clase 202 Desestructurar de undefine

--- seccion8/middlewares/index.js
+++ seccion8/middlewares/index.js
@@ -2,11 +2,13 @@
 const validarCampos =require('../middlewares/validar-campos.js');
 const  validarJWT  = require('../middlewares/validarJWT.js');
 const  validarRoles  = require('../middlewares/validar-roles.js');
+const validarArchivo = require ('../middlewares/validar-archivo.js')
 
 
 
 module.exports = {
     ...validarCampos,
     ...validarJWT,
-    ...validarRoles
+    ...validarRoles,
+    ...validarArchivo
 };
