996499207ff356c1784ea973728a946197d6290e
leydevitm <anelgarcia764@gmail.com>
2025-06-26 17:25:20 -0600

    Clase 233 Atender un ticket

--- seccion10/models/ticket-control.js
+++ seccion10/models/ticket-control.js
@@ -1,83 +1,89 @@
 const path = require('path');
-const fs= require('fs');
+const fs   = require('fs');
 
-class Ticket{
-    constructor(numero,escritorio){
-        this.numero =numero,
+class Ticket {
+    constructor( numero, escritorio ) {
+        this.numero = numero;
         this.escritorio = escritorio;
-
     }
 }
 
 
+class TicketControl {
 
 
+    constructor() {
+        this.ultimo   = 0;
+        this.hoy      = new Date().getDate(); // 11
+        this.tickets  = [];
+        this.ultimos4 = [];
 
-class TicketControl{
-
-    constructor(){
-        this.ultimo=0;
-        this.hoy = new Date();
-        this.tickets=[];
-        this.ultimos4=[];
-         this.init();
-
+        this.init();
     }
 
 
-    get toJson(){
+    get toJson() {
         return {
-        ultimo: this.ultimo,
-        hoy: this.hoy,
-        tickets: this.tickets,
-        ultimos4: this.ultimos4,
-       
+            ultimo: this.ultimo,
+            hoy: this.hoy,
+            tickets: this.tickets,
+            ultimos4: this.ultimos4,
         }
     }
 
-    init(){
-        const {hoy,tickets,ultimo,ultimos4} = require('../db/data');
-        if(hoy== this.hoy){
-            this.tickets = tickets,
-            this.ultimo=ultimo,
-            this.ultimos4=ultimos4
-        }else{
+    init() {
+        const { hoy, tickets, ultimo, ultimos4 } = require('../db/data.json');
+        if ( hoy === this.hoy ) {
+            this.tickets  = tickets;
+            this.ultimo   = ultimo;
+            this.ultimos4 = ultimos4;
+        } else {
+            // Es otro dia
             this.guardarDB();
         }
+    }
+
+    guardarDB() {
+
+        const dbPath = path.join( __dirname, '../db/data.json' );
+        fs.writeFileSync( dbPath, JSON.stringify( this.toJson ) );
 
     }
 
-    guardarDB(){
-        const dbPath = path.join(__dirname, '../db/data.json');
-        fs.writeFileSync(dbPath, JSON.stringify(this.toJson));
-
-    }
-
-    siguiete (){
-        this.ultimo +=1;
-        const ticket = new Ticket(this.ultimo);
-        this.tickets.push(ticket)
+    siguiente() {
+        this.ultimo += 1;
+        const ticket = new Ticket( this.ultimo, null );
+        this.tickets.push( ticket );
 
         this.guardarDB();
-        return 'Ticket' + ticket.numero;
+        return 'Ticket ' + ticket.numero;
     }
 
-    atenderTicket(escritorio){
-        if(this.tickets.length==0){
+    atenderTicket( escritorio ) {
+
+        // No tenemos tickets
+        if ( this.tickets.length === 0 ) {
             return null;
         }
-        const ticket = this.tickets.shift(); //this.tickets[0]
+
+        const ticket = this.tickets.shift(); // this.tickets[0];
         ticket.escritorio = escritorio;
 
-        this.ultimos4.unshift(ticket);
-        if(this.ultimos4.length >4){
-            this.ultimos4.splice(-1,1)  //para cortarlo, comienza en la ultima posicion y corta 1
+        this.ultimos4.unshift( ticket );
+
+        if ( this.ultimos4.length > 4 ) {
+            this.ultimos4.splice(-1,1);
         }
 
         this.guardarDB();
-        return ticket;
 
+        return ticket;
     }
+
+
+
 }
 
-module.exports=TicketControl;
+
+
+module.exports = TicketControl;
--- seccion10/public/js/escritorio.js
+++ seccion10/public/js/escritorio.js
@@ -1,3 +1,50 @@
+//REFERENCIAS HTML
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button');
+const lblTicket = document.querySelector('small');
+const divAlerta = document.querySelector('.alert');
 
 
-console.log('Escritorio HTML');
+const searchParams = new URLSearchParams(window.location.search);
+
+if(!searchParams.has('escritorio')){
+    windows.location = 'index.html';
+    throw new Error('El escritorio es obligatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText=escritorio;
+divAlerta.style.display='none';
+
+const socket = io();
+
+
+socket.on('connect', () => {
+    btnAtender.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnAtender.disabled = true;
+});
+
+socket.on('ultimo-ticket', (ultimo) => {
+    //lblNuevoTicket.innerText = 'Ticket ' + ultimo;
+})
+
+
+btnAtender.addEventListener( 'click', () => {
+   socket.emit('atender-ticket', {escritorio}, ({ok,ticket, msg})=>{
+    if(!ok){
+        lblTicket.innerText='Nadie';
+        return divAlerta.style.display ='';
+
+    }
+    lblTicket.innerText = 'Ticket' + ticket.numero;
+
+   })
+    // socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+    //     lblNuevoTicket.innerText = ticket;
+        //console.log('desde el servidor', ticket);
+    // });
+
+});
