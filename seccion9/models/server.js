b5d8a2b5b31930d95da73f9ba1f312773c3db089
leydevitm <anelgarcia764@gmail.com>
2025-06-25 19:12:21 -0600

    Clase 217 Configuracion de Socket.io

---
+++ seccion9/models/server.js
@@ -0,0 +1,56 @@
+
+const express = require('express')
+const cors = require('cors');
+// const { dbConnection } = require('../database/config');
+
+class Server{
+
+    constructor(){
+    this.app  = express()
+    this.port = process.env.PORT;
+    this.server = require('http').createServer(this.app);
+    this.io = require('socket.io')(this.server);
+
+    this.paths={};
+ 
+    // Middlewares
+    this.middlewares();
+    //rutas
+    this.routes();
+    //sockets
+    this.sockets();
+    }
+
+    
+    middlewares(){
+
+        //cors
+        this.app.use(cors());
+
+        //directorio publico 
+        this.app.use(express.static('public'));
+    
+    }
+
+    routes(){}
+
+    sockets(){
+        this.io.on('connection', socket =>{
+            console.log('Cliente conectado', socket.id);
+           // socket.disconect()
+
+           socket.on('disconnect' , ()=> {
+            console.log('Cliente Desconectado', socket.id);
+           })
+        });
+    }
+    
+        listen(){
+    this.server.listen(this.port, ()=>{
+    console.log('Servidor corriendo en el puerto', this.port);
+    });
+}
+}
+
+
+module.exports = Server;
