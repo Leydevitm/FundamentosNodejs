a26d2684203e9fa7b675f03840b51ea953ffac60
leydevitm <anelgarcia764@gmail.com>
2025-06-26 12:20:11 -0600

    Clase 229 Clase para centralizar la logica de los tickets

---
+++ seccion10/sockets/controller.js
@@ -0,0 +1,23 @@
+const TicketControl = require("../models/ticket-control");
+
+const ticketControl= new TicketControl();
+
+const socketController =(socket) =>{
+          
+           socket.on('disconnect' , ()=> {});
+           socket.on('enviar-mensaje',(payload, callback)=>{
+
+            const id =123456;
+            callback({id, fecha: new Date().getTime()});
+            //es envsiado y refleja el payload
+           socket.broadcast.emit('enviar-mensaje', payload)
+           });
+        }
+
+
+
+
+
+module.exports={
+    socketController
+}
