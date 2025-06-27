ba3d441921b6caf207eba4b6b36a2d1ead83e0dd
leydevitm <anelgarcia764@gmail.com>
2025-06-26 18:15:45 -0600

    Clase 236 Reproducir audio cuando se asigna un ticket

--- seccion10/sockets/controller.js
+++ seccion10/sockets/controller.js
@@ -9,14 +9,14 @@ const socketController = (socket) => {
     // Cuando un cliente se conecta
     socket.emit( 'ultimo-ticket', ticketControl.ultimo );
     socket.emit( 'estado-actual', ticketControl.ultimos4 );
-    //socket.emit( 'tickets-pendientes', ticketControl.tickets.length);
+    socket.emit( 'tickets-pendientes', ticketControl.tickets.length);
         
 
     socket.on('siguiente-ticket', ( payload, callback ) => {
         
         const siguiente = ticketControl.siguiente();
         callback( siguiente );
-       // socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length);
+       socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length);
 
     });
 
@@ -33,8 +33,8 @@ const socketController = (socket) => {
        //notificar cambio en los ultimos 4
         
         socket.broadcast.emit( 'estado-actual', ticketControl.ultimos4 );
-    //     socket.emit( 'tickets-pendientes', ticketControl.tickets.length);
-    //     socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length);
+        socket.emit( 'tickets-pendientes', ticketControl.tickets.length);
+       socket.broadcast.emit( 'tickets-pendientes', ticketControl.tickets.length);
 
         if ( !ticket ) {
             callback({
