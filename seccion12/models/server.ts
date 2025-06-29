7ab8b3a784290173aaff307d66ddaad1a59a4fe7
leydevitm <anelgarcia764@gmail.com>
2025-06-28 19:45:22 -0600

    Clase 258 Middlewares necesarios

--- seccion12/models/server.ts
+++ seccion12/models/server.ts
@@ -1,16 +1,27 @@
 
 import express, {Application} from 'express';
- 
+import userRoutes from '../routes/usuario';
 
  class Server {
      private app: Application;
      private port: string;
+     private apiPaths ={
+        usuario: '/api/usuarios'
+     }
 
 
     constructor(){
        this.app = express();
        this.port = process.env.PORT || '8000';
+       this.routes();
     }
+
+    routes(){
+        this.app.use(this.apiPaths.usuario, userRoutes)
+    }
+ 
+
+
     listen(){
         this.app.listen(this.port, ()=>{
             console.log('Servidor corriendo en el puerto ' + this.port);
