bee97fc99c9fe6d90e4ef419c82f7f0c2c871aa2
leydevitm <anelgarcia764@gmail.com>
2025-06-30 01:15:02 -0600

    Clase 284 Renderizar Mensajes

--- seccion13/public/js/socket-chat.js
+++ seccion13/public/js/socket-chat.js
@@ -1,16 +1,15 @@
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
+
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre y sala son necesarios es necesario');
+    throw new Error('El nombre y sala son necesarios');
 }
 
-
-var usuario ={
+var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-
 };
 
 
@@ -18,9 +17,11 @@ var usuario ={
 socket.on('connect', function() {
     console.log('Conectado al servidor');
 
-    socket.emit('entrarChat', usuario, function(resp){
-     console.log('Usuarios conectados', resp);
+    socket.emit('entrarChat', usuario, function(resp) {
+        //console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -33,7 +34,7 @@ socket.on('disconnect', function() {
 
 // Enviar información
 // socket.emit('crearMensaje', {
-//     usuario: 'Leivy',
+//     nombre: 'Fernando',
 //     mensaje: 'Hola Mundo'
 // }, function(resp) {
 //     console.log('respuesta server: ', resp);
@@ -41,22 +42,20 @@ socket.on('disconnect', function() {
 
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    //console.log('Servidor:', mensaje);
+    renderizarMensajes(mensaje, false);
+     scrollBottom();
 });
 
-//Escuchar cambios de usuarios
-//cuando un usuario entra o sale del chat 
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersona', function(personas) {
-
-    console.log( personas);
-
+    renderizarUsuarios(personas);
 });
 
-//mensajes privados
-socket.on('mensajePrivado',function(mensaje){
- console.log('Mensaje Privado:' , mensaje);
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
 
- 
+    console.log('Mensaje Privado:', mensaje);
+
 });
